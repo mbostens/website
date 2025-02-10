92663237f4482ae5c9d1383ae2bcd23f091bf5e9
mbostens <mbostens@gmail.com>
2025-02-11 00:41:58 +0100

    Fix template path issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask_cors import CORS
 from flask import render_template, Flask
-
+import os
 # Flask app setup
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +8,8 @@ CORS(app)
 
 @app.route('/')
 def index():
+    print("Current Working Directory:", os.getcwd())
+    print("Template search path:", app.jinja_loader.searchpath)
     return render_template("index.html")
 
 # Run the Flask app
